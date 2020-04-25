fc5b2e35717d444e325bd9e02632b171b887f0cd
zscgrhg <zscgrhg@qq.com>
2020-04-26 01:24:50 +0800

    perfect!++

--- run-spec.groovy
+++ run-spec.groovy
@@ -1,37 +1,44 @@
 import com.zte.sputnik.extension.BMRuleLoader
 
 import java.nio.file.Paths
+import java.util.stream.Stream
 
-try {
-    System.setProperty("org.jboss.byteman.home","${project.build.directory}/sputnik-lib")
 
+def runSpec(File pkg){
     def antBuilder=new groovy.util.AntBuilder()
-    antBuilder.mkdir(dir: Paths.get(project.build.directory).resolve("report"))
-    antBuilder.path(id:'sputnik.test.classpath'){
-        pathelement (path:project.getTestClasspathElements())
-    }
-    antBuilder.path(id:'sputnik.compile.classpath'){
-        pathelement (path:project.getCompileClasspathElements())
-    }
+
     antBuilder.junitlauncher(printSummary:true){
 //        classpath (refid:'sputnik.compile.classpath')
 //        classpath (refid:'sputnik.test.classpath')
         classpath {
             //${project.build.directory}/sputnik-lib
-            pathelement (path:Paths.get(project.build.directory).resolve("sputnik-classes/pkg0"))
-            pathelement (path:"${project.build.directory}/sputnik-lib")
-            pathelement (path:project.getCompileClasspathElements())
+            pathelement (path:pkg.toPath())
             pathelement (path:project.getTestClasspathElements())
 
         }
         testclasses(outputdir:Paths.get(project.build.directory).resolve("report")){
-            fileset(dir: Paths.get(project.build.directory).resolve("sputnik-classes/pkg0")){
+            fileset(dir: pkg.toPath()){
                 include(name:"**/*SpecTest.class")
             }
-            listener( type:"legacy-brief", sendSysOut:"true")
-            listener( type:"legacy-xml", sendSysErr:"true", sendSysOut:"true")
+            listener( type:"legacy-brief", sendSysOut:"false")
+            listener( type:"legacy-xml", sendSysErr:"false", sendSysOut:"false")
         }
     }
+}
+
+try {
+    def antUtil=new groovy.util.AntBuilder()
+    antUtil.mkdir(dir: Paths.get(project.build.directory).resolve("report"))
+
+    //System.setProperty("org.jboss.byteman.home","${project.build.directory}/sputnik-lib")
+    def pkgs = Paths.get(project.build.directory).resolve("sputnik-classes").toFile().listFiles()
+
+    Stream.of(pkgs)
+    .forEach({pkg->
+        runSpec(pkg)
+    })
+
+
 } catch (Throwable e) {
     e.printStackTrace()
 }
